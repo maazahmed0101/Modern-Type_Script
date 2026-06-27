7a82cb9eaaad9ad925c765de666c394dd9f7c43a
maazahmed0101 <wwwmaazahmed1@gmail.com>
2026-06-28 00:14:45 +0500

    update

---
+++ Advanced-Typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return person + 'hello';
+}
+let obj = greet('maaz');
+console.log(obj);
+//# sourceMappingURL=index.js.map
---
+++ Advanced-Typescript/src/index.ts
@@ -0,0 +1,5 @@
+function greet(person:string):string{
+    return person + 'hello'
+}
+let obj = greet('maaz')
+console.log(obj);
